03d6501cbdce640518570aee11ddd13e5570a422
aron ujvary <aronujvary@gmail.com>
2017-09-05 17:01:57 +0200

    signal i3lock when img is changed

--- src/sh/run_if_locked.sh
+++ src/sh/run_if_locked.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
-pgrep i3lock >/dev/null
+proc=$(pgrep "i3lock")
 rv=$?
+echo $proc $rv
 if [ $rv -eq 0 ]; then
 	/home/aujvary/src/sh/changelockscreen.sh
+	for p in $(echo $proc); do
+		kill -SIGUSR2 $p
+	done
 fi
 
